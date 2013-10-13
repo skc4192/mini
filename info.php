49882ae84ce4bb4fd20a645b771ea79ac41f1b57
David Menache <dave@doap.com>
2013-10-13 08:57:02 -0600

    Update info.php

--- info.php
+++ info.php
@@ -1,3 +1,21 @@
+<html>
+  <head>
+    <title>
+      DevOps and Platforms
+    </title>
+  </head>
+  <body>
+    <h2>DevOps and Platforms</h2>
+    <P>Welcome to another Doap website</p>
+      <ul>
+        <li>Repo is here: <a href="https://github.com/Doap/mini">GitHub Mini repo</a></a></li>
+        <li>Click <a href="index.php">here</a> to try some php.</li>
+      </ul>
+      <h3>Php info:</h3>
+      <pre>
+      <?php echo phpinfo(); ?>
+      </pre>
+  </body>
+</html>
 
 
-<?php echo phpinfo(); ?>
