db3391d193b588a89e3c032b3546b1c71c60121d
David Menache <dave@doap.com>
2013-10-13 19:16:38 -0600

    Update doap.php

--- doap.php
+++ doap.php
@@ -1,10 +1,160 @@
-<div style="position:relative;float:right">
-<img src="custom/themes/default/images/company_logo.png?v=08B3jpfsRY7WsrVHzTs_tQ&amp;logo_md5=d455b302e47d575c3070fe6950291526" width="200" height="40" alt="Company logo" border="0">
+<div style="position:relative;float:right;margin-right:50px;">
+<img src="http://doapcrm.doap.com/doapcrm/custom/themes/default/images/company_logo.png" width="200" height="40" alt="Company logo" border="0">
 </div>
-DevOps and Platforms
-Comprehensive cloud solutions
+
+
+
+DevOps and Platforms<br>
+Comprehensive cloud solutions<br>
 
 <a href=http://www.doap.com>Enter</a>
+ 
+ <div id="wrapper" class="clearfix">
+				
+<div id="content" class="grid col-620">
+        
+	
+		        
+        <div class="breadcrumb-list" xmlns:v="http://rdf.data-vocabulary.org/#"><span typeof="v:Breadcrumb"><a rel="v:url" property="v:title" href="http://doap.com/">Home</a></span> <span class="chevron">›</span> <span class="breadcrumb-current">Web hosting</span></div>        
+						<div id="post-35" class="post-35 page type-page status-publish hentry">       
+				
+                
+<h1 class="entry-title post-title">Web hosting</h1>
+
+                 
+                <div class="post-entry">
+                    <p><strong></strong></p><div class="menu-container"><ul id="menu-networks" class="menu"><li id="menu-item-71" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71"><strong><a href="http://doap.com/email/">Email</a></strong></li><strong>
+<li id="menu-item-74" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-35 current_page_item menu-item-74"><a href="http://doap.com/web-hosting/">Web</a></li>
+<li id="menu-item-72" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-72"><a href="http://doap.com/databases/">Databases</a></li>
+<li id="menu-item-73" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-73"><a href="http://doap.com/servers/">Servers</a></li>
+<li id="menu-item-70" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-70"><a href="http://doap.com/pbx-systems/">Voice</a></li>
+</strong></ul></div><strong>&nbsp;</strong><p></p>
+<p><br class="Apple-interchange-newline">Doap.com is the home of the “The Doap Theme”, a generic yet feature packed WordPress framework.</p>
+<p><img class=" wp-image-191 alignleft" style="margin-right: 10px;" alt="doapy128sq" src="http://doap.com/wp-content/uploads/2013/05/doapy128sq.png" width="168" height="168"><br>
+Out of the box it comes with awesome social network integration elements like Facebook and Google+ that go far beyond what most +Oauth2 sites are offering these days. &nbsp;This and other great functionality is already baked right into your site! &nbsp;It’s DOAP and it just works. &nbsp;Test drive your very own fully functional system here for free! *Normal bandwidth restrictions apply.</p>
+
+<table id="tablepress-4" class="tablepress tablepress-id-4">
+<caption style="caption-side:bottom;text-align:left;border:none;background:none;margin:0;padding:0;"><a href="http://doap.com/wp-admin/admin.php?page=tablepress&amp;action=edit&amp;table_id=4">Edit</a></caption>
+<thead>
+<tr class="row-1 odd">
+	<th class="column-1"><div>&nbsp;</div></th><th class="column-2"><div>Price</div></th>
+</tr>
+</thead>
+<tbody class="row-hover">
+<tr class="row-2 even">
+	<td class="column-1"><div style="position:relative;float:left;"><img src="http://www.doap.com/wp-content/uploads/2013/05/doap-final-logo.png" width="50"></div><br>
+<div style="position:relative;float:left;"><h2>Free 1yr Website!</h2><h3>Light traffic self service website.</h3><br>
+<h5>Fully functional generic Wordpress website hosted at http://yourbiz.doap.com</h5><br>
+</div></td><td class="column-2"><h5>*<a href="http://doap.com/contact-us/" title="Get started by clicking here. With Doap.com, you can launch a free Wordpress website into Amazons vast Virtual Cloud">Free</a></h5><br>
+<br>
+<form method="post" id="tcp_frm_714" action="http://doap.com/shopping-cart/checkout/"><br>
+<div class="tcp_buy_button tcp_buy_button_simple" style="position:relative;float:right;"><br>
+					<div class="tcp-add-to-cart"><br>
+				<input type="hidden" name="tcp_count[]" id="tcp_count_714" value="1" class="input-mini tcp_count"><br>
+					<input type="hidden" name="tcp_post_id[]" id="tcp_post_id_714" value="714"><br>
+	<img src="http://doap.com/wp-admin/images/loading.gif" class="tcp_feedback" id="tcp_buy_button_feedback_714" style="display: none;">			<div class="tcp-add-to-wishlist"><br>
+							</div><br>
+				<span class="tcp_added_product_title tcp_added_product_title_714"><br>
+		</span><br>
+	<script type="text/javascript"><br />
+tcpDispatcher.add( 'tcp_items_in_the_cart_714', 714 );<br />
+<br />
+function tcp_items_in_the_cart_714() {<br />
+	jQuery.getJSON(<br />
+		"http://doap.com/wp-admin/admin-ajax.php",<br />
+		{<br />
+			action : 'tcp_shopping_cart_actions',<br />
+			to_do : 'get_items_in_the_cart',<br />
+			post_id : 714,<br />
+		} ).done( function( response ) {<br />
+			jQuery( '.tcp_added_product_title_714' ).replaceWith( response );<br />
+		} ).fail( function( error ) {<br />
+			alert( error.responseText );<br />
+		} );<br />
+}<br />
+</script><!-- HOLA --><br>
+				</div><br>
+		</div><br>
+</form></td>
+</tr>
+<tr class="row-3 odd">
+	<td class="column-1">Light traffic fully managed</td><td class="column-2">$25.00/mo.</td>
+</tr>
+<tr class="row-4 even">
+	<td class="column-1">High traffic sites</td><td class="column-2">published rates apply</td>
+</tr>
+</tbody>
+</table>
+
+                                    </div><!-- end of .post-entry -->
+            
+				
+           
+
+<div class="post-edit"><a class="post-edit-link" href="http://doap.com/wp-admin/post.php?post=35&amp;action=edit" title="Edit Page">Edit</a></div>  				               
+				      
+			</div><!-- end of #post-35 -->       
+			            
+						
+
+
+
+			            
+          
+      
+</div><!-- end of #content -->
+
+
+<div id="widgets" class="grid col-300 fit">
+			
+		    
+<!-- 
+	Plugin: Custom Meta Widget	Plugin URL: http://shinraholdings.com/plugins/custom-meta-widget/-->
+
+
+	<div id="custommetawidget-2" class="widget-wrapper customMetaWidget">	<div class="widget-title">Hiya</div>	<ul>
+	
+	
+    <li><a href="http://doap.com/wp-admin/">Site Admin</a></li><li><a href="http://doap.com/wp-login.php?action=logout&amp;_wpnonce=a710437e51">Log out</a></li><li><a href="http://doap.com/feed/" title="Syndicate this site using RSS 2.0">Entries <abbr title="Really Simple Syndication">RSS</abbr></a></li><li><a href="http://doap.com/comments/feed/" title="Syndicate this site using RSS 2.0">Comments <abbr title="Really Simple Syndication">RSS</abbr></a></li><li><a href="http://www.doap.com">Doap.com</a></li>	
+	</ul>
+    
+	</div><div id="text-5" class="widget-wrapper widget_text"><div class="widget-title">Personal VPC</div>			<div class="textwidget">Thats right get a free VPC fully configured and hosted in the cloud.  Your VPC includes this great infrastructure:
+<p>
+1 External Firewall<br>
+3 Internal Firewalls<br>
+1 Class B Private Network<br>
+     10.0.0.0/16<br>
+3 Class C Private Subnets<br>
+     10.0.0.0/24<br>
+      10.0.1.0/24<br>
+      10.0.2.0/24<br>
+3 Public IP Addresses<br>
+1 Load balancer<br>
+1 Mysql Database<br>
+3 Linux Servers<br>
+     1 Web Server<br>
+     *1 NAT Device<br>
+     *1 Bastion Server<br>
+SSL Encryption Key Installed<br>
+SSH Direct Root Access </p><p>
+
+*Bastion and NAT servers are provisioned in an 'Off' state and running them will incur standard Micro instance charges for each.
+
+</p></div>
+		</div>
+	</div><!-- end of #widgets -->
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-41092906-1']);
+_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+		    </div>
  
 Web & App Hosting
 Automated scaling, performance monitoring, and redundancy.
