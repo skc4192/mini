b264fd5b8477a450624de0a597a06e430f5da5b5
David Menache <dave@doap.com>
2013-10-13 18:55:05 -0600

    Create doap.php

---
+++ doap.php
@@ -0,0 +1,15 @@
+<div style="position:relative;float:right">
+<img src="custom/themes/default/images/company_logo.png?v=08B3jpfsRY7WsrVHzTs_tQ&amp;logo_md5=d455b302e47d575c3070fe6950291526" width="200" height="40" alt="Company logo" border="0">
+</div>
+DevOps and Platforms
+Comprehensive cloud solutions
+
+<a href=http://www.doap.com>Enter</a>
+ 
+Web & App Hosting
+Automated scaling, performance monitoring, and redundancy.
+Email for Business
+We will implement a Google Apps domain for your organization.
+
+We build and host custom CRM systems for businesses of any size.
+Customer Agreement ToS AUP SLA Support Guidelines Privacy Philosophy
